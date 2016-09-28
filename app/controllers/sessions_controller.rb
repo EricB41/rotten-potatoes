0816c4e03877a4bdd75b6e338b9dae57df85db94
Eric B <ericbechtold4@hotmail.com>
2016-09-28 22:44:46 +0000

    Fixed the login issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
     def user_params
-        params.require(:login).permit(:user_id, :email)
+        params.require(:session).permit(:user_id, :email)
     end
     
     def create
-        if User.find_by_user_id(params[:login][:user_id]) && User.find_by_email(params[:login][:email])
-            @user = User.find_by_user_id(params[:login][:user_id])
+        if User.find_by_user_id(params[:session][:user_id]) && User.find_by_email(params[:session][:email])
+            @user = User.find_by_user_id(params[:session][:user_id])
             flash[:notice] = "You are logged in as #{@user.user_id}"
             session[:session_token] = @user.session_token
             redirect_to movies_path
