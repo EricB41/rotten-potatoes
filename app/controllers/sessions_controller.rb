85150b449a66215438bef4c4ea50a6f70165b9d3
Eric B <ericbechtold4@hotmail.com>
2016-09-30 19:19:40 +0000

    Made a couple of fixes to comply with requirements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,8 @@ class SessionsController < ApplicationController
     end
     
     def create
-        if User.find_by_user_id(params[:session][:user_id]) && User.find_by_email(params[:session][:email])
-            @user = User.find_by_user_id(params[:session][:user_id])
-            flash[:notice] = "You are logged in as #{@user.user_id}"
+        @user = User.find_by_user_id(params[:session][:user_id])
+        if @user && (@user.email == params[:session][:email])
             session[:session_token] = @user.session_token
             redirect_to movies_path
         else
